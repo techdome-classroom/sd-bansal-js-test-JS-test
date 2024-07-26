7ceb233510eecc77335817f85fe879a550c1c139
Pushakr <pushkarsharma9131@gmail.com>
2024-07-26 13:02:57 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,25 +1,9 @@
 const decodeTheRing = function (s, p) {
-  // write your code here
 
-  const assert = require("assert");
-  const decodeTheRing = require("./program2");
+    // write your code here
 
-  describe("Test cases for decodeTheRing function", function () {
-    it(`Returns true for "hello", "h*llo"`, function () {
-      const result = decodeTheRing("hello", "h*llo");
-      assert.equal(result, true);
-    });
+    
 
-    it(`Returns false for "secret", "sec?"`, function () {
-      const result = decodeTheRing("secret", "sec?");
-      assert.equal(result, false);
-    });
-
-    it(`Returns false for "codeword", "c*de"`, function () {
-      const result = decodeTheRing("codeword", "c*de");
-      assert.equal(result, false);
-    });
-  });
-};
-
-module.exports = decodeTheRing;
+  };
+  
+  module.exports = decodeTheRing;
