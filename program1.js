2667912370ed27d1993fde185153f5ba77f09848
Pushakr <pushkarsharma9131@gmail.com>
2024-07-26 12:24:11 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,28 +1,34 @@
 const getTotalIsles = function (grid) {
-  let islands = 0;
-  const rows = grid.length;
-  const cols = grid[0].length;
+  // if (!grid || grid.length === 0) return 0;
 
-  function dfs(i, j) {
-    if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] === "0") {
+  let total_island = 0;
+
+  const row = grid.length;
+  const col = grid[0].length;
+
+  const dfs = (i, j) => {
+    if (i < 0 || j < 0 || i >= numRows || j >= numCols || grid[i][j] === "W")
       return;
-    }
 
-    grid[i][j] = "0";
+    grid[i][j] = "W"; // Mark this cell as visited
 
-    return dfs(i + 1, j) || dfs(i - 1, j) || dfs(i, j + 1) || dfs(i, j - 1);
-  }
+    // Visit all adjacent cells (up, down, left, right)
+    dfs(i - 1, j); // Up
+    dfs(i + 1, j); // Down
+    dfs(i, j - 1); // Left
+    dfs(i, j + 1); // Right
+  };
 
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === "1") {
-        islands += 1;
-        dfs(i, j);
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      if (grid[i][j] === "L") {
+        totalIsles++;
+        dfs(i, j); // Start a DFS to mark all parts of this island
       }
     }
   }
 
-  return islands;
+  return totalIsles;
 };
 
 module.exports = getTotalIsles;
