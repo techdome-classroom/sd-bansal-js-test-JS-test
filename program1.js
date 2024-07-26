ab44a8a1ca6c6f58e4bb353700a2fcc040f04287
Pushakr <pushkarsharma9131@gmail.com>
2024-07-26 12:28:06 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,35 +1,4 @@
 const getTotalIsles = function (grid) {
-  // if (!grid || grid.length === 0) return 0;
-
-  // let to;tal_island = 0;
-
-  // const row = grid.length;
-  // const col = grid[0].length;
-
-  // const dfs = (i, j) => {
-  //   if (i < 0 || j < 0 || i >= numRows || j >= numCols || grid[i][j] === "W")
-  //     return;
-
-  //   grid[i][j] = "W"; // Mark this cell as visited
-
-  //   // Visit all adjacent cells (up, down, left, right)
-  //   dfs(i - 1, j); // Up
-  //   dfs(i + 1, j); // Down
-  //   dfs(i, j - 1); // Left
-  //   dfs(i, j + 1); // Right
-  // };
-
-  // for (let i = 0; i < numRows; i++) {
-  //   for (let j = 0; j < numCols; j++) {
-  //     if (grid[i][j] === "L") {
-  //       totalIsles++;
-  //       dfs(i, j); // Start a DFS to mark all parts of this island
-  //     }
-  //   }
-  // }
-
-  // return totalIsles
-
   let islands = 0;
   const rows = grid.length;
   const cols = grid[0].length;
