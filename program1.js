f661abc998db94fa86424402fd19c3858f7a35fb
Pushakr <pushkarsharma9131@gmail.com>
2024-07-26 12:24:42 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -29,6 +29,31 @@ const getTotalIsles = function (grid) {
   // }
 
   // return totalIsles;
+
+  let islands = 0;
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  function dfs(i, j) {
+    if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] === "0") {
+      return;
+    }
+
+    grid[i][j] = "0";
+
+    return dfs(i + 1, j) || dfs(i - 1, j) || dfs(i, j + 1) || dfs(i, j - 1);
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === "1") {
+        islands += 1;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return islands;
 };
 
 module.exports = getTotalIsles;
