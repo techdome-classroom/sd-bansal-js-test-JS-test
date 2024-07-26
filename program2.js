71cc5a096c4df8f876c2308a74225ce895345251
vivekraj singh thakur <vsinghthakur187@gmail.com>
2024-07-26 12:32:07 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -23,7 +23,7 @@ const decodeTheRing = function (s, p) {
       }
   }
   
-  // Fill the DP table
+ 
   for (let i = 1; i <= m; i++) {
       for (let j = 1; j <= n; j++) {
           if (p[j - 1] === '*') {
