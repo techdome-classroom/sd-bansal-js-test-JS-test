0fcb112d8a170fa8420f4845c0ac784811fd48cf
Pushakr <pushkarsharma9131@gmail.com>
2024-07-26 13:04:53 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,18 +1,18 @@
 const decodeTheRing = function (s, p) {
   // write your code here
 
-  if (s.length !== pattern.length) {
+  if (s.length !== p.length) {
     return false;
   }
 
-  // Loop through each character in the string and pattern
+  // Loop through each character in the string and p
   for (let i = 0; i < s.length; i++) {
-    // If the pattern character is '*', skip to the next character
-    if (pattern[i] === "*") {
+    // If the p character is '*', skip to the next character
+    if (p[i] === "*") {
       continue;
     }
     // If the characters do not match, return false
-    if (s[i] !== pattern[i]) {
+    if (s[i] !== p[i]) {
       return false;
     }
   }
