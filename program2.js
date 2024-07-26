4dfb538ab9aff0c04b43d8a01c589353e2a02be2
vivekraj singh thakur <vsinghthakur187@gmail.com>
2024-07-26 12:32:16 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -14,9 +14,8 @@ const decodeTheRing = function (s, p) {
   const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(false));
   
   // Base case
-  dp[0][0] = true; // Both empty
-  
-  // Handle patterns with leading *
+  dp[0][0] = true;// Both empty
+ 
   for (let j = 1; j <= n; j++) {
       if (p[j - 1] === '*') {
           dp[0][j] = dp[0][j - 1];
